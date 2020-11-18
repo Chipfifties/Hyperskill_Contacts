b34bf601a0b25e0d4fc8bf6b92d027f353e4651c
Ilia <65903212+Chipfifties@users.noreply.github.com>
2020-11-19 01:29:19 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package contacts;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ContactsApp app = new ContactsApp(getPath(args));
+        app.run();
+    }
+
+    private static String getPath(String[] args) {
+        if (args.length != 0) {
+            return args[1];
+        }
+        return "contacts.db";
+    }
+}
